9d399dfd2403d26f0f7c47a2fb6338f03e21ab6f
Mohammad Akhlaghi <akhlaghi@gnu.org>
2015-04-06 21:27:43 +0900

    Started work on Header
    Header is a program to print and manipulate the header information in
    a data file.

---
+++ src/header/ui.c
@@ -0,0 +1,325 @@
+/*********************************************************************
+Header - View and manipulate a data file header
+Header is part of GNU Astronomy Utilities (Gnuastro) package.
+
+Original author:
+     Mohammad Akhlaghi <akhlaghi@gnu.org>
+Contributing author(s):
+Copyright (C) 2015, Free Software Foundation, Inc.
+
+Gnuastro is free software: you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation, either version 3 of the License, or (at your
+option) any later version.
+
+Gnuastro is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with gnuastro. If not, see <http://www.gnu.org/licenses/>.
+**********************************************************************/
+#include <config.h>
+
+#include <math.h>
+#include <stdio.h>
+#include <errno.h>
+#include <error.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fitsio.h>
+
+#include "timing.h"	/* Includes time.h and sys/time.h   */
+#include "checkset.h"
+#include "txtarrayvv.h"
+#include "commonargs.h"
+#include "configfiles.h"
+#include "fitsarrayvv.h"
+
+#include "main.h"
+
+#include "ui.h"		        /* Needs main.h                   */
+#include "args.h"	        /* Needs main.h, includes argp.h. */
+
+
+/* Set the file names of the places where the default parameters are
+   put. */
+#define CONFIG_FILE SPACK CONF_POSTFIX
+#define SYSCONFIG_FILE SYSCONFIG_DIR "/" CONFIG_FILE
+#define USERCONFIG_FILEEND USERCONFIG_DIR CONFIG_FILE
+#define CURDIRCONFIG_FILE CURDIRCONFIG_DIR CONFIG_FILE
+
+
+
+
+
+
+
+
+
+
+/**************************************************************/
+/**************       Options and parameters    ***************/
+/**************************************************************/
+void
+readconfig(char *filename, struct headerparams *p)
+{
+  FILE *fp;
+  size_t lineno=0, len=200;
+  char *line, *name, *value;
+  /*struct uiparams *up=&p->up;*/
+  struct commonparams *cp=&p->cp;
+  char key='a';	/* Not used, just a place holder. */
+
+  /* When the file doesn't exist or can't be opened, it is ignored. It
+     might be intentional, so there is no error. If a parameter is
+     missing, it will be reported after all defaults are read. */
+  fp=fopen(filename, "r");
+  if (fp==NULL) return;
+
+
+  /* Allocate some space for `line` with `len` elements so it can
+     easily be freed later on. The value of `len` is arbitarary at
+     this point, during the run, getline will change it along with the
+     pointer to line. */
+  errno=0;
+  line=malloc(len*sizeof *line);
+  if(line==NULL)
+    error(EXIT_FAILURE, errno, "ui.c: %lu bytes in readdefaults",
+	  len * sizeof *line);
+
+  /* Read the tokens in the file:  */
+  while(getline(&line, &len, fp) != -1)
+    {
+      /* Prepare the "name" and "value" strings, also set lineno. */
+      STARTREADINGLINE;
+
+
+
+
+      /* Inputs: */
+      if(strcmp(name, "hdu")==0)
+	{
+	  if(cp->hduset) continue;
+	  errno=0;
+	  cp->hdu=malloc(strlen(value)+1);
+	  if(cp->hdu==NULL)
+	    error(EXIT_FAILURE, errno, "Space for HDU.");
+	  strcpy(cp->hdu, value);
+	  cp->hduset=1;
+	}
+
+
+
+      /* Outputs */
+
+
+
+
+      /* Operating modes: */
+      else if(strcmp(name, "numthreads")==0)
+	{
+	  if(cp->numthreadsset) continue;
+	  sizetlzero(value, &cp->numthreads, name, key, SPACK,
+		     filename, lineno);
+	  cp->numthreadsset=1;
+	}
+
+
+      else
+	error_at_line(EXIT_FAILURE, 0, filename, lineno,
+		      "`%s` not recognized.\n", name);
+    }
+
+  free(line);
+  fclose(fp);
+}
+
+
+
+
+
+void
+printvalues(FILE *fp, struct headerparams *p)
+{
+  /*struct uiparams *up=&p->up;*/
+  struct commonparams *cp=&p->cp;
+
+  /* Print all the options that are set. Separate each group with a
+     commented line explaining the options in that group. */
+  fprintf(fp, "\n# Input image:\n");
+  if(cp->hduset)
+    {
+      if(stringhasspace(cp->hdu))
+	fprintf(fp, CONF_SHOWFMT"\"%s\"\n", "hdu", cp->hdu);
+      else
+	fprintf(fp, CONF_SHOWFMT"%s\n", "hdu", cp->hdu);
+    }
+}
+
+
+
+
+
+
+/* Note that numthreads will be used automatically based on the
+   configure time. */
+void
+checkifset(struct headerparams *p)
+{
+  /*struct uiparams *up=&p->up;*/
+  struct commonparams *cp=&p->cp;
+
+  int intro=0;
+  if(cp->hduset==0)
+    REPORT_NOTSET("hdu");
+
+
+  END_OF_NOTSET_REPORT;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**************************************************************/
+/***************       Sanity Check         *******************/
+/*************************************************************
+void
+sanitycheck(struct headerparams *p)
+{
+
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**************************************************************/
+/***************       Preparations         *******************/
+/*************************************************************
+void
+preparearrays(struct headerparams *p)
+{
+
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**************************************************************/
+/************         Set the parameters          *************/
+/**************************************************************/
+void
+setparams(int argc, char *argv[], struct headerparams *p)
+{
+  struct commonparams *cp=&p->cp;
+
+  /* Set the non-zero initial values, the structure was initialized to
+     have a zero value for all elements. */
+  cp->spack         = SPACK;
+  cp->verb          = 1;
+  cp->numthreads    = DP_NUMTHREADS;
+  cp->removedirinfo = 1;
+
+  /* Read the arguments. */
+  errno=0;
+  if(argp_parse(&thisargp, argc, argv, 0, 0, p))
+    error(EXIT_FAILURE, errno, "Parsing arguments");
+
+  /* Add the user default values and save them if asked. */
+  CHECKSETCONFIG;
+
+  /* Check if all the required parameters are set. */
+  checkifset(p);
+
+  /* Print the values for each parameter. */
+  if(cp->printparams)
+    REPORT_PARAMETERS_SET;
+
+  /* Make the array of input images.
+  preparearrays(p);
+  */
+  /* Do a sanity check.
+  sanitycheck(p);
+  */
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**************************************************************/
+/************      Free allocated, report         *************/
+/**************************************************************/
+void
+freeandreport(struct headerparams *p, struct timeval *t1)
+{
+  /* Free the allocated arrays: */
+  free(p->cp.hdu);
+  free(p->cp.output);
+
+  if(p->wcs)
+    wcsvfree(&p->nwcs, &p->wcs);
+}
