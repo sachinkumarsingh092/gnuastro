46fbcd1d62c767f6acb4f503a147a94ddbec5741
Mosè Giordano <mose@gnu.org>
2015-11-07 11:34:41 +0100

    Add Emacs dir locals file

---
+++ .dir-locals.el
@@ -0,0 +1,9 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil
+  (indent-tabs-mode . nil)
+  (fill-column . 80))
+ (c-mode
+  (c-basic-offset . 2)
+  (c-file-style . "gnu")))
