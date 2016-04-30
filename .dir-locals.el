8d77077bc416550d849cc9f699435b08e19b63bb
Mosè Giordano <mose@gnu.org>
2016-04-30 19:24:24 +0200

    More information about dir-local file
    Add more comments into .dir-local.el file describing what the file is
    and does.
    
    Describe in manual the presence of the file and how it is useful for
    Emacs users.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,9 +1,17 @@
-;;; Directory Local Variables
-;;; For more information see (info "(emacs) Directory Variables")
+;; This files contains Emacs Directory Local Variables.
+;;
+;; Emacs is an extensible, customizable, free/libre text editor.  It allows
+;; specification of certain settings that will be applied to all files in
+;; current directory and its subdirectories.  This is useful in order to
+;; automatically enforce certain coding conventions for all contributors of
+;; Gnuastro , like the maximum length of lines or the number of spaces to be
+;; used for indentation.
+;;
+;; For more information see (info "(emacs) Directory Variables")
 
 ((nil
-  (indent-tabs-mode . nil)
-  (fill-column . 80))
+  (indent-tabs-mode . nil) ;; No tabs as indentation
+  (fill-column . 80)) ;; 80-character wide lines
  (c-mode
-  (c-basic-offset . 2)
-  (c-file-style . "gnu")))
+  (c-basic-offset . 2) ;; 2 spaces of indentation
+  (c-file-style . "gnu"))) ;; GNU style for braces
