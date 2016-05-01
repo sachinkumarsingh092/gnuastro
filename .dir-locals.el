0c2d802ce91ddd9213b81ee3021f51b7c6adf923
Mohammad Akhlaghi <akhlaghi@gnu.org>
2016-05-01 22:19:28 +0900

    Development: Elaboration on last two conventions
    Some extra explanations were added in the end of the "Coding
    conventions" section of the book explaining the basic reasoning behind
    the last two conventions (about spacing between the functions: 5 lines
    for similar functions and 20 lines for groups of similar functions).

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,11 +1,11 @@
 ;; This files contains Emacs Directory Local Variables.
 ;;
-;; Emacs is an extensible, customizable, free/libre text editor.  It allows
-;; specification of certain settings that will be applied to all files in
-;; current directory and its subdirectories. This is useful in order to
-;; automatically enforce certain coding conventions for all contributors of
-;; Gnuastro, like the maximum length of lines or the number of spaces to be used
-;; for indentation.
+;; Emacs is an extensible, customizable, free/libre text editor.  It
+;; allows specification of certain settings that will be applied to
+;; all files in current directory and its subdirectories. This is
+;; useful in order to automatically enforce certain coding conventions
+;; for all contributors of Gnuastro, like the maximum length of lines
+;; or the number of spaces to be used for indentation.
 ;;
 ;; For more information see (info "(emacs) Directory Variables")
 
