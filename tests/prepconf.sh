1f5587425002e7b52d1adcaf44416db57937d0c6
Mohammad Akhlaghi <akhlaghi@gnu.org>
2016-05-03 12:00:16 +0900

    Installation: Only local configuration files in `make check'
    Until now, when `make check' was run, the utilities would also go
    looking into the user and system-wide directories for options. The
    `onlydirconf' option is now added to the configuration file of all the
    utilities at testing time so the utilities only look into
    tests/.gnuastro/ for configuration files. The system was also set in
    place to add other such options specific to `make check' in the future
    if need be.
    
    Checking user and system configuration files caused problems when the
    newer version of a utility has changed old options (renamed, or
    removed them). Although this should not happen too often, but in major
    releases and during the first few years of Gnuastro (while it is
    stabilizing) it might happen. So the checks would fail with the
    respective utility complaining of an unrecognized option.

--- tests/prepconf.sh
+++ tests/prepconf.sh
@@ -1,29 +1,78 @@
 # This is not actually a test of any of the programs, it just brings
 # in all the configuration files into a locally created .gnuastro
-# directory for all the tests to use.
+# directory for all the tests to use. It is part of the GNU Astronomy
+# Utilities (Gnuastro).
 #
 # Original author:
 #     Mohammad Akhlaghi <akhlaghi@gnu.org>
 # Contributing author(s):
+# Copyright (C) 2016, Free Software Foundation, Inc.
 #
-# Copying and distribution of this file, with or without modification,
-# are permitted in any medium without royalty provided the copyright
-# notice and this notice are preserved.  This file is offered as-is,
-# without any warranty.
+# Gnuastro is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
+#
+# Gnuastro is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Gnuastro. If not, see <http://www.gnu.org/licenses/>.
 
 
-# Make the .gnuastro directory if not already created.
+# Make the .gnuastro directory
+# ----------------------------
+#
+# This directory will keep all the configuration files for this run of
+# `make check'.
 if [ ! -d .gnuastro ]; then
     mkdir .gnuastro
 fi
 
 
-#For each program bring in the configuration file:
+
+
+
+# Add options for `make check'
+# ----------------------------
+#
+# It might be necessary to add specific options to all the programs during
+# `make check'. Therefore we have defined the file `addedoptions.txt' to
+# keep these extra options and append them to the configuration file in the
+# source directory of the utility.
+#
+#   - The onlydirconf option is added so the utilities don't go looking
+#     into the user's home and system wide directories (which might contain
+#     configuration files from older versions). If the option names have
+#     changed or an option has been removed, such sitations will cause a
+#     failed test.
+cat > addedoptions.txt <<EOF
+
+
+# Added only for "make check":
+onlydirconf           1
+EOF
+
+
+
+
+
+# Bring utility configuration files
+# ---------------------------------
+#
+# Each utility's configuration file is read and appended with the
+# addedoptions.txt file to create the configuration file which will be used
+# by `make check'.
 for prog in arithmetic convertt convolve cosmiccal header imgcrop \
             imgstat imgwarp mkcatalog mknoise mkprof noisechisel  \
             subtractsky
 do
-    if [ ! -f .gnuastro/ast$prog.conf ]; then
-	cp $topsrc/src/$prog/ast$prog.conf .gnuastro/
-    fi
+
+    # Copy the configuration file from the utility source and add the
+    # options added here.
+    cat $topsrc/src/$prog/ast$prog.conf addedoptions.txt          \
+        > .gnuastro/ast$prog.conf
+
 done
