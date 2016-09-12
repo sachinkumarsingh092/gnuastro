79ed5321a55ccd63daf0a90e66854d464c8dc168
Mohammad Akhlaghi <akhlaghi@gnu.org>
2016-09-12 14:20:29 +0200

    Corrected information in polygon library source
    `lib/polygon.c' and `lib/gnuastro/polygon.h' were initially written as part
    of ImageWarp and then moved to the libraries. After the move, the top
    commented information were missed (from correction) and still mentioned
    ImageWarp. They are now corrected.

--- lib/polygon.c
+++ lib/polygon.c
@@ -1,6 +1,6 @@
 /*********************************************************************
-ImageWarp - Warp images using projective mapping.
-ImageWarp is part of GNU Astronomy Utilities (Gnuastro) package.
+Polygon related functions and macros.
+This is part of GNU Astronomy Utilities (Gnuastro) package.
 
 Original author:
      Mohammad Akhlaghi <akhlaghi@gnu.org>
