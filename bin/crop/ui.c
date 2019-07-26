f8af58e67f1f1ba563d79c985b5246e27df3c88d
Mohammad Akhlaghi <mohammad@akhlaghi.org>
2019-07-26 12:27:52 +0100

    Installation: unused parts removed from configure script
    In the previous commit, we used a more robust library linking test for the
    configure script which removed the need for several large blocks of of the
    `configure' script.
    
    With this commit, those extra blocks are deleted from the configure script
    for clarity and cleanness.

--- bin/crop/ui.c
+++ bin/crop/ui.c
@@ -937,6 +937,7 @@ ui_preparations(struct cropparams *p)
       if(p->mode==IMGCROP_MODE_WCS) wcsmode_check_prepare(p, img);
     }
 
+
   /* Polygon cropping is currently only supported on 2D */
   if(p->imgs->ndim!=2 && p->polygon)
     error(EXIT_FAILURE, 0, "%s: polygon cropping is currently only "
